9c7a8b00c935884a3622c2d505fef15a7cc6566d
liuhongyu33218 <liuhongyu33218@gmail.com>
2023-04-09 22:17:34 -0400

    style update

---
+++ mosesmapbox/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXRzbGl1aG9uZ3l1IiwiYSI6ImNsZzhucDF6ZDBnZzgzbnFsN215cXB6NjgifQ.RRT5nI_kklSehZ7ZsRJpmA';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/itsliuhongyu/clg8lek7o00dh01mqsb93akfm',
+    zoom: 5,
+    center: [-74, 40.725]
+});
